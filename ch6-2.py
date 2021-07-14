fe7c969f1a2645e3072dd1ff6f462260954826fb
EibonV <812528931@qq.com>
2021-07-14 11:36:54 +0800

    增加6-8

--- ch6-2.py
+++ ch6-2.py
@@ -23,3 +23,43 @@ person3 = {
 people = [person,person2,person3]
 for info in people:
     print(f"\n{info['first name'].title()} {info['last name'].title()} is {info['age']} years old,living in {info['city'].title()} city")
+
+#6-8
+#新建列表
+pets = []
+
+#新建宠物信息并储存到pets
+pet = { 
+    'animal type': 'python', 
+    'name': 'john', 
+    'owner': 'guido', 
+    'weight': '43', 
+    'eats': 'bugs' 
+    }
+pets.append(pet)
+
+pet = {
+    'animal type': 'chicken', 
+    'name': 'clarence', 
+    'owner': 'tiffany', 
+    'weight': '2', 
+    'eats': 'seeds' 
+    }
+pets.append(pet)
+
+pet = { 
+    'animal type': 'dog', 
+    'name': 'peso', 
+    'owner': 'eric', 
+    'weight': '37', 
+    'eats': 'shoes' 
+    }
+pets.append(pet)
+
+#遍历列表，把宠物信息打出来
+for info in pets:
+    print(f"\n{info['name'].title()} is a {info['animal type']},{info['weight']} kilometers,it's {info['owner'].title()}'s,it eats {info['eats']}.")
+for info in pets:
+    print(f"\nHere is about {info['name'].title()}:")
+    for key,value in info.items():
+        print(f"\t{key}:{value}")
